04a9f5f02273cd325dc4bdaaa664f4a99185c85c
Eyvind Malde <eyvind.malde@gmail.com>
2022-10-14 23:40:31 +0200

    Initial commit

---
+++ Oblig3-Handleliste/src/main/java/no/hvl/dat108/Oblig3/Handleliste/Oblig3HandlelisteApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.Oblig3.Handleliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3HandlelisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig3HandlelisteApplication.class, args);
+	}
+
+}
---
+++ Oblig3-Handleliste/src/test/java/no/hvl/dat108/Oblig3/Handleliste/Oblig3HandlelisteApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.Oblig3.Handleliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3HandlelisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
