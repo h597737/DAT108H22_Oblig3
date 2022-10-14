f4c3f35f7690f2f9de6feeea27566a8c94ee2aec
Eyvind Malde <eyvind.malde@gmail.com>
2022-10-14 23:46:05 +0200

    Initial commit for del 2

---
+++ Oblig3-Terning/src/main/java/no/hvl/dat108/Oblig3/Terning/Oblig3TerningApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.Oblig3.Terning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3TerningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig3TerningApplication.class, args);
+	}
+
+}
---
+++ Oblig3-Terning/src/test/java/no/hvl/dat108/Oblig3/Terning/Oblig3TerningApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.Oblig3.Terning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3TerningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
